6ed56bdedc48c6478a34373cfaf553550e326e6b
Adrian Reyer <are@lihas.de>
2016-11-09 18:29:40 +0100

    setup_ffsconfiggenerator.sh with GWSUBID

--- setup_ffsconfiggenerator.sh
+++ setup_ffsconfiggenerator.sh
@@ -11,18 +11,21 @@ fi
 python -c '
 import json, sys
 GWID='$GWID'
+GWSUBID='$GWSUBID'
 fp = open("config.json","rb")
 config = json.load(fp)
 fp.close()
 if ( "$GWID" not in config["gws"] ):
   fp = open("config.json","wb")
   config["gws"][GWID] = {}
-  config["gws"][GWID]["legacyipv4"] = "'$LEGIP'"
-  config["gws"][GWID]["legacyipv6"] = "fd21:b4dc:4b1e::a38:'$GWLID'"
-  config["gws"][GWID]["externalipv4"] = "'$EXT_IP_V4'"
-  config["gws"][GWID]["externalipv6"] = "'$EXT_IPS_V6'"
-  config["gws"][GWID]["ipv4start"] = "172.21.'$((4*$GWID))'.2"
-  config["gws"][GWID]["ipv4end"] = "172.21.'$((4*$((GWID+1))-1))'.254"
+  config["gws"][GWID]["instance"] = {}
+  config["gws"][GWID]["instance"][GWSUBID] = {}
+  config["gws"][GWID]["instance"][GWSUBID]["legacyipv4"] = "'$LEGIP'"
+  config["gws"][GWID]["instance"][GWSUBID]["legacyipv6"] = "fd21:b4dc:4b1e::a38:'$GWLID'"
+  config["gws"][GWID]["instance"][GWSUBID]["externalipv4"] = "'$EXT_IP_V4'"
+  config["gws"][GWID]["instance"][GWSUBID]["externalipv6"] = "'$EXT_IPS_V6'"
+  config["gws"][GWID]["instance"][GWSUBID]["ipv4start"] = "172.21.'$((4*$GWID))'.2"
+  config["gws"][GWID]["instance"][GWSUBID]["ipv4end"] = "172.21.'$((4*$((GWID+1))-1))'.254"
   json.dump(config, fp, indent=2)
   fp.close()
 '
